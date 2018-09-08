1e17eb98bac750b9a361a128d8e8a1a2d8494ed6
Matthew <matthewghgriffiths@gmail.com>
2018-09-08 09:44:16 +0100

    Modifying hardshell.f90

--- nestedbasinsampling/structure/fortran/hardshell.f90
+++ nestedbasinsampling/structure/fortran/hardshell.f90
@@ -28,20 +28,18 @@ integer, intent(in) :: natoms
 double precision, intent(in) :: coords(3*natoms), radius
 double precision, intent(out) :: e, grad(3*natoms)
 
-double precision dr(3), r, r2, r2cut
+double precision r, r2, r2cut
 integer j
 
 r2cut = radius**2
 
 e = 0.d0
 grad(:) = 0.d0
-do j=1, natoms
-    dr(:) = coords(3*(j-1)+1 : 3*(j-1) + 3)
-    r2 = sum(dr(:)**2)
+do j=1,3*natoms-2,3
+    r2 = coords(j)**2 + coords(j+1)**2 + coords(j+2)**2
     if (r2 > r2cut) then
-        !r = sqrt(r2)
-        e = e + (r2 - r2cut)/2 !r - radius
-        grad(3*(j-1)+1 : 3*(j-1) + 3) = coords(3*(j-1)+1 : 3*(j-1) + 3)!/r
+        e = e + (r2 - r2cut)/2
+        grad(j:j+2) = coords(j:j+2)
     endif
 enddo
 
