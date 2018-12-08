c97b6be0775a993b55c2b95d3b740a773e6294f8
Matthew <matthewghgriffiths@gmail.com>
2018-12-08 12:14:14 +0000

    fixing seed in interface

--- nestedbasinsampling/sampling/noguts.py
+++ nestedbasinsampling/sampling/noguts.py
@@ -517,7 +517,6 @@ class NoGUTSWalker(NoGUTSSampler):
     """Interface to work with nested_sampling/sens
     """
     def __call__(self, x0, stepsize, Emax, energy, seed=None):
-        self.seed = seed
         super(NoGUTSWalker, self).__call__(Emax, x0, stepsize=stepsize)
 
 
